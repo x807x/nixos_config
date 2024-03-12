2de903a7c0781c758bbdd882529600f4d9f8b470
ckeisc807 <s10855168@gmail.com>
2024-03-12 18:14:16 +0800

    Tue Mar 12 06:14:16 PM CST 2024

--- configuration.nix
+++ configuration.nix
@@ -19,6 +19,8 @@
   networking.hostName = "807nix"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
