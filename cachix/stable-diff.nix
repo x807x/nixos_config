8a3f8231c8301cb6e525d8c4f88af21d591c3510
ckeisc807 <s10855168@gmail.com>
2024-03-28 01:17:14 +0800

    import cachix

---
+++ cachix/stable-diff.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://stable-diff.cachix.org"
+      ];
+      trusted-public-keys = [
+        "stable-diff.cachix.org-1:liYFm3f3q1dAoilj2Ag2IEKzW3Q9/HJcLlrAIytAcy0="
+      ];
+    };
+  };
+}
