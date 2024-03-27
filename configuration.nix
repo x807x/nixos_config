9bf452d025acb6ed85b4f051fee99ec6374d2e36
ckeisc807 <s10855168@gmail.com>
2024-03-28 01:39:50 +0800

    Thu Mar 28 01:39:50 CST 2024

--- configuration.nix
+++ configuration.nix
@@ -217,6 +217,7 @@
     oxygen
 	kwallet
   ];
+
   environment.systemPackages = with pkgs; [
     nano# Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     neofetch
