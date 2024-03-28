18cc1e1392f6e349aca565210afdb1f8afdef6d3
ckeisc807 <s10855168@gmail.com>
2024-03-29 07:55:04 +0800

    ignore system monitor

--- configuration.nix
+++ configuration.nix
@@ -80,7 +80,7 @@
 
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
   # GNOME desktop intergration
@@ -232,6 +232,7 @@
   # $ nix search wget
   environment.plasma5.excludePackages = with pkgs.libsForQt5; [
     plasma-browser-integration
+    plasma-systemmonitor
     oxygen
 	kwallet
   ];
