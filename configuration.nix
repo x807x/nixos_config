ea186345eab76f14d212e88c478f2e7e3ba230f4
ckeisc807 <s10855168@gmail.com>
2024-03-29 18:44:06 +0800

    add vieb

--- configuration.nix
+++ configuration.nix
@@ -218,6 +218,7 @@
 
 
 	environment.systemPackages = with pkgs; [
+		vieb
 		auto-cpufreq
 			nano# Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
 			neofetch
